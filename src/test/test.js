b2fabfffd33b9aaf72dbc2d3684e9519f04d1ec2
Denis Vorobev <Kurtcobaine192@outlook.com>
2023-07-27 20:37:11 +0300

    fix eslint error

--- src/test/test.js
+++ src/test/test.js
@@ -1,17 +1,17 @@
-import getHealth from "../hero";
+import getHealth from '../hero';
 
-test("health", () => {
-  const hero = { name: "Маг", health: 90 };
+test('health', () => {
+  const hero = { name: 'Маг', health: 90 };
   const result = getHealth(hero);
-  expect(result).toBe("healthy");
+  expect(result).toBe('healthy');
 });
-test("wounded", () => {
-  const hero = { name: "Маг", health: 40 };
+test('wounded', () => {
+  const hero = { name: 'Маг', health: 40 };
   const result = getHealth(hero);
-  expect(result).toBe("wounded");
+  expect(result).toBe('wounded');
 });
-test("critical", () => {
-  const hero = { name: "Маг", health: 5 };
+test('critical', () => {
+  const hero = { name: 'Маг', health: 5 };
   const result = getHealth(hero);
-  expect(result).toBe("critical");
+  expect(result).toBe('critical');
 });
